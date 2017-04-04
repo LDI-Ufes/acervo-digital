83e0093684b770e01bae89dfc7c442ffc430158d
Eduardo França <edox86@gmail.com>
2017-04-04 17:11:48 -0300

    Erro em classe do Bootstrap
    Mais um erro tosco, troquei "col-md" por "cold-md", por isso o layout estava ainda mais zoado do que deveria. Corrigido.

--- resources/views/courses/form.blade.php
+++ resources/views/courses/form.blade.php
@@ -1,7 +1,7 @@
 
 <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
 	<label for="name" class="col-md-2 control-label">Nome</label>
-	<div class="cold-md-10">
+	<div class="col-md-10">
 		<input class="form-control" name="name" type="text" id="name" value="{{ old('name', isset($course) ? $course->name : null) }}" maxlength="80">
 		{!! $errors->first('name', '<p class="help-block">:message</p>') !!}
 	</div>
@@ -9,7 +9,7 @@
 
 <div class="form-group {{ $errors->has('modules') ? 'has-error' : ''}}">
 	<label for="modules" class="col-md-2 control-label">Módulos</label>
-	<div class="cold-md-10">
+	<div class="col-md-10">
 		<input class="form-control" name="modules" type="text" id="modules" value="{{ old('modules', isset($course) ? $course->modules : null) }}" maxlength="80">
 		{!! $errors->first('modules', '<p class="help-block">:message</p>') !!}
 	</div>

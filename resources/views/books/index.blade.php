9f6d49ff525af0e6d7923ac178f5cbd2d96990cc
renato-ldi <renatodelpupo@designinstrucional.org>
2017-04-10 11:44:33 -0300

    Páginas de Livros
    Listagem e criação de livros. / Inclusão da paleta dos cursos (cursos.less)

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,22 +1,59 @@
 @extends('layouts.app')
-
 @section('content')
-	<div class="container">
-		<div class="row">
-		        <div class="col-md-8 col-md-offset-2">
-		            <div class="panel panel-default">
-		                <div class="panel-heading">Listagem de Cursos</div>
 
-				<div class="panel-body">
-					<ul>
-						@foreach( $all_books as $book)
-							<li> {{ $book->title }} [{{$book->course->name }}] <a href="{{ route('books.show', $book->id) }}">[ver]</a> <a href="{{ route('books.edit', $book->id) }}">[editar]</a> </li>
-						@endforeach
-					</ul>
-				</div>
+<!-- CABEÇALHO -->
+<section class="content-header">
+    <h1>
+        Livros
+        <small>Listagem</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><i class="fa fa-book"></i> Livros</li>
+        <li class="active"><i class="fa fa-list"></i> Listagem</li>
+    </ol>
+</section>
+<!-- FIM CABEÇALHO -->
 
-			    </div>
-			</div>
-		</div<>
-	</div>
+<!-- TABELA - LISTA DE LIVROS -->
+<section class="content">
+    <div class="row">
+        <section class="listaLivros col-md-12 connectedSortable ui-sortable">
+            <div class="box box-ldi">
+                <div class="box-header">
+                    <div>
+                        <h3 class="box-title">
+                            <i class="fa fa-book"></i> Livros cadastrados
+                        </h3>
+                    </div>
+                    <hr style="margin-bottom:0;">
+                </div>
+
+                <div class="box-body table-responsive">
+                    <table id="projecttable" class="table table-bordered table-hover table-striped" width="100%">
+                        <thead>
+                            <tr>
+                                <th>Curso</th>
+                                <th>Livro</th>
+                                <th class="icone"></th>
+                                <th class="icone"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                          @foreach( $all_books as $book)
+                          <tr>
+                            <td class="box-curso" title="{{$book->course->name }}"></td>
+                            <td>{{ $book->title }}</td>
+                            <td><a href="{{ route('books.show', $book->id) }}"><i class="fa fa-eye"></i></a></td>
+                            <td><a href="{{ route('books.edit', $book->id) }}"><i class="fa fa-edit"></i></a></td>
+                          </tr>
+                          @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
+    </div>
+</section>
+<!-- FIM TABELA - LISTA DE LIVROS -->
+
 @endsection

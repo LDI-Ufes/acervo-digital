1692c7ce767148c1a17b378942b6d00592b0586c
Eduardo França <edox86@gmail.com>
2017-05-18 16:44:43 -0300

    Acomodandos os novos esquema no banco
    Modificações necessárias para criar as tabelas para os novos tipos no banco de dados.

--- database/migrations/2017_05_17_164109_create_course_types_table.php
+++ database/migrations/2017_05_17_164109_create_course_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCoursesTable extends Migration
+class CreateCourseTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateCoursesTable extends Migration
      */
     public function up()
     {
-        Schema::create('courses', function(Blueprint $table){
+        Schema::create('course_types', function (Blueprint $table) {
 		$table->increments('id');
 		$table->string('name');
-		$table->integer('modules');
+
 		$table->timestamps();
 	});
     }
@@ -28,6 +28,6 @@ class CreateCoursesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('courses');
+	Schema::dropIfExists('course_types');
     }
 }
---
+++ database/migrations/2017_05_17_164130_create_courses_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	if (!Schema::hasTable('courses')) {
+	        Schema::create('courses', function(Blueprint $table){
+			$table->increments('id');
+	
+			$table->integer('type_id')->unsigned();
+			$table->foreign('type_id')->references('id')->on('course_types');
+	
+			$table->string('name');
+			$table->integer('modules');
+			$table->string('bg_color');
+			$table->string('fg_color');
+			$table->string('aux_color');
+			$table->char('short', 3);
+	
+			$table->timestamps();
+		});
+	}
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}

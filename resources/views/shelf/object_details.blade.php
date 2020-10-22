0851ab40a6cb932f10f892dadac71e9e0d276955
Eduardo França <edox86@gmail.com>
2020-10-22 13:59:28 -0400

    Copiei parte do esqueleto da página de acervo-v2
    Ainda há varias questões a resolver. Como o breadcrumb (porque, como
    cada material pode estar em vários cursos, não dá para saber pelo
    material de que curso o aluno veio. Tenho que pensar em uma forma de
    resolver isso). Não há informação sobre ISBN no banco também. Não
    existem as "tags" de informação ainda. Nem a possibilidade de vários
    materiais cadastrados em cada item (do Learning Objects).
    Mas com as infomações e página para cada material, dá para ir trazendo o
    CSS de acervo-v2 e ir incorporando a nova estrutura proposta.

--- resources/views/shelf/object_details.blade.php
+++ resources/views/shelf/object_details.blade.php
@@ -2,21 +2,45 @@
 
 @section('content')
 
+  <section class="breadcrumb">
+    Você está em: <a href="#">???</a> <i class="fas fa-greater-than"></i><span>{{ $learning_object->title }}</span>
+  </section>
+
+  <div class="conteudo-material">
+    <span class="tipo"> {{ $learning_object->type->name  }} </span>
+  </div>
+
+  <h1 class="titulo"> {{ $learning_object->title }} </h1>
+  <p class="autor"> {{ $learning_object->author }} </p>
+  <p class="edicao"> Edição ??? (não há no banco) </p>
+
+  <div class="acesso">
+    <a class="btn-primario" href="{{ $learning_object->link }}" target="_blank"><i class="fa fa-file-pdf"></i>PDF? Interativo? Livro?</a>
+  </div>
+
+  <blockquote class="caixa-info">
+    <h2>Inscruções de acesso</h2>
+    <p>As instruções de acesso vão aqui.</p>
+  </blockquote>
+
+
+  <hr />
+  <hr />
+
   <div>
-                  <ul>
-                <li><b>Curso:</b> {{ $learning_object->course->pluck('short')->implode(' ') }}</li>
-                <!--<li><b>Módulo:</b> {{ $learning_object->module }}</li>-->
-                <li><b>Título:</b> {{ $learning_object->title }}</li>
-                <li><b>Autor:</b> {{ $learning_object->author }}</li>
-		            <li><b>Ano:</b> {{ $learning_object->year }}</li>
-                <li><b>Endereço:</b> {{ $learning_object->link }}</li>
-                <li>
-                  <b>Resumo</b> <br>
-                  {{ $learning_object->summary }}
-                </li>
-              </ul>
-
-
+    <!-- alguns exemplos de como pegar as informações do banco de dados -->
+    <ul>
+      <li><b>Curso:</b> {{ $learning_object->course->pluck('short')->implode(' ') }}</li>
+      <li><b>Título:</b> {{ $learning_object->title }}</li>
+      <li><b>Autor:</b> {{ $learning_object->author }}</li>
+      <li><b>Ano:</b> {{ $learning_object->year }}</li>
+      <li><b>Endereço:</b> {{ $learning_object->link }}</li>
+      <li><b>Tipo de Material:</b> {{ $learning_object->type->name  }} </li>
+      <li>
+        <b>Resumo</b> <br>
+        {{ $learning_object->summary }}
+      </li>
+    </ul>
   </div>
 
 @endsection

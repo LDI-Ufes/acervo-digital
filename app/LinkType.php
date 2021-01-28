9a1551061e26dc34ee91d69ba90a365ad8d8ff78
Eduardo França <edox86@gmail.com>
2021-01-28 08:56:30 -0500

    Multiplos links para cada material
    Criando a primeira parte dos links, que é uma tabela define quais tipos
    de links existem, que texto aparece na descrição do botão daquele tipo
    de link (call-to-action), e um ícone associado ao tipo de link. Falta
    editar o controlador e criar a CRUD na parte adiministrativa.

---
+++ app/LinkType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LinkType extends Model
+{
+
+  /*
+   * remover comentário para ligar LinkType a tabela ResourceLink
+   * return $this->hasMany('App\ResourceLink');
+   */
+
+}
